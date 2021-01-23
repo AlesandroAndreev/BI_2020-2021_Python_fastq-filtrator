8bc786edc0480f93e87bdc8f9631119f86296806
Alexander Andreev <alexander@mail.ru>
2021-01-24 02:00:01 +0300

    Version 1.2. Now its fast

--- fastq-filtrator/filtrator.py
+++ fastq-filtrator/filtrator.py
@@ -14,31 +14,51 @@ def filtrator(size_param, contig_size, flag_write):
 
 def seq_size(first_string):
     size_string = re.search(r"length=[-+]?\d+",first_string)[0]
-
     return int(re.search(r"[-+]?\d+",size_string)[0])
 
+def gc_counter(seq,gc_parametr,flag_write):
+    gc_bases = 0
+
+    for nucl in seq:
+        if nucl in ["G","C"]:
+            gc_bases += 1
+
+    if (len(seq)/gc_bases)*100 > gc_parametr:
+        flag_write = 1
+
+    return flag_write
+
+
+fastq = open(way_to_read , 'r')
+file = open(way_to_write + '.fastq', 'w')
 
 
 
-with open(way_to_read,"r") as fastq:
+for lines in fastq:
 
-    for lines in fastq:
+    if "length" in lines:
+        if filtrator(size_param, seq_size(lines), flag_write) != 1:
+            flag_write = filtrator(size_param, seq_size(lines), flag_write)
+            file.write(lines + '\n')
+    elif r"^[A|C|T|G][A|C|T|G]" in lines:
+        pass
 
-        print(lines)
-
-        if "length" in lines:
-            flag_write =  filtrator(size_param, seq_size(lines),flag_write)
-
+        
         if flag_write != 1:
+            file.write(lines + '\n')
 
-            with open(way_to_write,"a") as fastq_sort:
-                fastq_sort.write(lines)
-
-
-
+file.close()
+fastq.close()
 
 
 
-#/Users/alexandreev/Desktop/amp_res_1.fastq
-#/Users/alexandreev/Desktop/ans.fastq
-#100
+
+
+
+'''
+        
+/Users/alexandreev/Desktop/amp_res_1.fastq
+/Users/alexandreev/Desktop/ans
+101
+
+'''
