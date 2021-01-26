3c4d000f5f605c16853666d1ea1ce236841c9c48
Alexander Andreev <alexander@mail.ru>
2021-01-27 00:03:28 +0300

    Version 2.0 with mistake

--- fastq-filtrator/filtrator.py
+++ fastq-filtrator/filtrator.py
@@ -4,6 +4,8 @@ import os, sys
 #_________________________________________________Ввод_переменных______________________________________________________
 way_to_read = input("Write the way to read: ")
 way_to_write = input("Write the way to write: ")
+way_to_error = input("Write the way to error: ")
+err_file_flag = input("Do you want file for wrong reads. Write True or False:")
 size_param = int(input("Write size: "))
 
 
@@ -11,78 +13,98 @@ maximum = int(input("Верхняя граница: "))
 minimum = int(input("Нижняя граница: "))
 
 
-flag_write = 0
+
 to_write = ""
 len_of_string = 0
+sum_of_all_gc_nucleotides = 0
+summarize_length_of_seq = 0
+flag_write = True
 #_________________________________________________Функции______________________________________________________
-
-
-def filtrator(size_param, contig_size): #Функция фильтрует по размеру
-
-    flag = 0
-
-    if contig_size < size_param:
-        flag = 1
-
-    return flag
-
-
-def seq_size(first_string): #Функция узнает какой размер у фрагмента
-    size_string = re.search(r"length=[-+]?\d+",first_string)[0]
-    return int(re.search(r"[-+]?\d+",size_string)[0])
-
-
-def gc_counter(seq, gc_max=100, gc_min=0): #Функция определяет gc состав
-
-    gc_bases = 0
-
-    for nucl in seq:
-        if nucl in ["G","C"]:
-            gc_bases += 1
-
-    if gc_min <= (gc_bases/len(seq))*100 <= gc_max:
-        flag = 0
+def filter_by_length(length_limit, read_header):
+    if length_limit < read_header:
+        return False
     else:
-        flag = 1
+        return True
 
-    return flag
+def count_gc_content_in_line(seq):
 
+    gc_nucleotides = 0
+
+    for nucleotide in seq:
+        if nucleotide in ["G","C"]:
+            gc_nucleotides += 1
+    return gc_nucleotides, len(seq) - 1
+
+
+def content_of_gc_nucleotides(all_gc_nucleotides, length_of_seq):
+    return (all_gc_nucleotides/length_of_seq)*100
+
+
+def filter_by_gc_content(content, gc_max=100, gc_min=0):
+    if gc_min <= content <= gc_max:
+        return True
+    else:
+        return False
 #_________________________________________________Тело_кода_____________________________________________________________
 
 
 fastq = open(way_to_read , 'r')
 file = open(way_to_write + '.fastq', 'w')
 
+if err_file_flag is True:
+    file_error = open(way_to_error + '.fastq','w')
+
 for lines in fastq:
 
-    if "length" in lines:
-        flag_write = filtrator(size_param, seq_size(lines))
-        len_of_string = len(lines)
-
-    elif re.search(r"^[A|C|T|G|N][A|C|T|G|N]",lines):
-        flag_write = gc_counter(lines, maximum, minimum)
-
-        if flag_write == 1:
-            to_write = to_write[0:len(to_write) - len_of_string ]
-
-    if flag_write != 1:
-        to_write = to_write + lines
-        if re.search(r"^@", lines) and "length" not in lines:
+    if ("length" in lines) and (len(to_write) > 1):
+        if flag_write is True:
             file.write(to_write)
             to_write = ""
+            len_of_string = 0
+            sum_of_all_gc_nucleotides = 0
+            summarize_length_of_seq = 0
+        elif (flag_write is False) and (err_file_flag is True):
+            file_error.write(to_write)
+            to_write = ""
+            len_of_string = 0
+            sum_of_all_gc_nucleotides = 0
+            summarize_length_of_seq = 0
+            file_error.close()
+        else:
+            to_write = ""
+            len_of_string = 0
+            sum_of_all_gc_nucleotides = 0
+            summarize_length_of_seq = 0
+
+
+    elif re.search(r"^[A|C|T|G|N][A|C|T|G|N]",lines) and "+" not in to_write:
+        sum_of_all_gc_nucleotides += count_gc_content_in_line(lines)[0]
+        summarize_length_of_seq += count_gc_content_in_line(lines)[1]
+
+    elif re.search(r"^[+]",lines):
+        gc_content = content_of_gc_nucleotides( sum_of_all_gc_nucleotides, summarize_length_of_seq)
+        flag_write = (filter_by_length(size_param, summarize_length_of_seq) and filter_by_gc_content(gc_content, maximum,minimum))
+
+
+    to_write += lines
+
+
+
+try:
+    file_error.close()
+except:
+    pass
 
 file.close()
 fastq.close()
 
 
 
-
-
-
 '''
-        
+
 /Users/alexandreev/Desktop/amp_res_1.fastq
 /Users/alexandreev/Desktop/ans
+/Users/alexandreev/Desktop/ans_err
 101
 /Users/alexandreev/Desktop/test.fastq
 '''
